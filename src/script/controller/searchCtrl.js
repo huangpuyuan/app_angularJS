d357807e12ffcef36b06d8216a86e9df81bf8d26
huangpuyuan <huangpuyuan@outlook.com>
2017-03-20 17:30:07 +0800

    new

--- src/script/controller/searchCtrl.js
+++ src/script/controller/searchCtrl.js
@@ -23,13 +23,43 @@ angular.module('app').controller('searchCtrl', ['dict', '$scope', '$http', funct
         name: '公司规模'
     }];
 
+    var tabId = '';
+    $scope.filterObj={};
+
     $scope.tClick = function(id, name) {
+    	tabId = id;
         $scope.sheet.list = dict[id];
         $scope.sheet.visible = true;
     };
 
-    // $scope.sClick = function(id, name) {
-    //    console.log(id,name);
-    // };
+    $scope.sClick = function(id, name) {
+       console.log(id,name);
+       if(id){
+       		angular.forEach($scope.tabList,function(item){
+       			if(item.id===tabId){
+       				item.name =name;
+       			}
+       		});
+       		$scope.filterObj[tabId +'Id'] = id;
+       }else{
+       		delete $scope.filterObj[tabId +'Id'];
+       		angular.forEach($scope.tabList,function(item){
+       			if(item.id===tabId){
+       				switch(item.id){
+       					case 'city': 
+       						item.name = '城市';
+       					 break;
+	   					case 'salary': 
+	   						item.name = '薪水';
+	   					 break;
+	   					case 'scale': 
+	   						item.name = '公司规模';
+	   				 	 break;	
+	   				 	default:
+       				}
+       			}
+       		})
+       }
+    };
 
 }]);
